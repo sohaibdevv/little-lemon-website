ba8e5003ef1ef205b4429286958c193c8ba1273f
Sohaib Malik <sohaibmalikccm@gmail.com>
2025-03-10 08:00:39 +0500

    updated layout files

--- README.md
+++ README.md
@@ -2,9 +2,14 @@
 
 # Little Lemon 🍋 🤠
 
-## Live Demo
+## Live Demo: [Here!]("https://little-lemon-sohaibdevv.netlify.app/")
 
-## Description
 Welcome to the **Little Lemon Restaurant** website repository. This project was created as a part of my learning journey of **Meta Frontend Developer Professional Certificate Program.** This Project consist of all the the skills and knowledge that i have gained throw the Professional Certificate Program. It Offers a stylish User interface for users who want to explore the restaurant's menu and make reservations.
 
+## Features: 🙂
+*
+*
+*
+
 ## Technologies Used
+[![My Skills](https://skillicons.dev/icons?i=html,css,js,react,figma,git)](https://skillicons.dev)
--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,13 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { HashLink } from "react-router-hash-link";
 import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import logoImage from "../assets/logo.png";
 import "./header.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = ({ navLinks }) => {
   const [isNavExpanded, setIsNavExpanded] = useState(false);
+  const location = useLocation();
+  const [activePath, setActivePath] = useState(
+    localStorage.getItem("activePath") || location.pathname
+  );
+
+  useEffect(() => {
+    setActivePath(localStorage.getItem("activePath") || location.pathname);
+  }, [location.pathname]);
+
+  const handleNavLinkClick = (path) => {
+    setIsNavExpanded(false);
+    setActivePath(path);
+    localStorage.setItem("activePath", path);
+  };
 
   return (
     <header>
@@ -33,9 +47,11 @@ const Header = ({ navLinks }) => {
           {navLinks.map((navLink) => (
             <li
               key={navLink.name}
-              onClick={() => setIsNavExpanded(false)}
+              onClick={() => handleNavLinkClick(navLink.path)}
               aria-label="On Click"
-              className="hover-underline-animation"
+              className={`hover-underline-animation ${
+                activePath === navLink.path ? "active" : ""
+              }`}
             >
               {navLink.hashLink ? (
                 <HashLink to={navLink.path}>{navLink.name}</HashLink>
